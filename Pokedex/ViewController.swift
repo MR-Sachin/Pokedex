aa225b9b22315dab2479ed23442fffe067f4c9c2
vishal chaubey <Vishal@sachins-macbook-pro.local>
2020-03-18 15:53:56 +0530

    add images of pokemon and csv file which a parsing image of pokemon into collection view cell

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate,UICollectionViewDataSource,UICollectionViewDelegateFlowLayout{
 
+    @IBOutlet weak var collection: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+        // let charmander = Pokemon(name: "Charmander", pokedexId: 4)
+        
+        collection.delegate = self
+        collection.dataSource = self
+        
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
+            return cell
+        } else {
+           return UICollectionViewCell()
+        }
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 30
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    
 
 
 }
